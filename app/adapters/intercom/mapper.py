bce8d6c4cfd7a49bc4ade1f1cc336bf60affb738
Vincentius Adi Kurnianto <vinceadi42@gmail.com>
2026-01-06 14:30:38 +0700

    implement adapter function to convert to InternalConversation

--- app/adapters/intercom/mapper.py
+++ app/adapters/intercom/mapper.py
@@ -1,6 +1,48 @@
 from app.models.external.intercom import IntercomConversationRaw
-from app.models.internal.conversation import InternalConversation
+from uuid import uuid4
+from datetime import datetime
+from app.models.internal.conversation import InternalParticipant, InternalMessage, InternalConversation
 
 
 def map_intercom_to_internal(payload: IntercomConversationRaw) -> InternalConversation:
+    payload_as_dict = payload.model_dump()
+    id = uuid4()
+    provider = "intercom" #TO DO: convert to Enums
+    external_id = payload_as_dict["id"]
+    created_at = datetime.fromtimestamp(payload_as_dict["created_at"])
+    updated_at = datetime.fromtimestamp(payload_as_dict["updated_at"])
+    participants = [InternalParticipant(
+        id=payload_as_dict["conversation_message"]["author"]["id"],
+        type=payload_as_dict["conversation_message"]["author"]["type"],
+        name=payload_as_dict["conversation_message"]["author"]["name"],
+        email=payload_as_dict["conversation_message"]["author"]["email"],
+    )] + [InternalParticipant(
+        id=cp["author"]["id"],
+        type=cp["author"]["type"],
+        name=cp["author"]["name"],
+        email=cp["author"]["email"],
+    ) for cp in payload_as_dict["conversation_parts"]["conversation_parts"]]
+    messages = [InternalMessage(
+            id=payload_as_dict["conversation_message"]["id"],
+            author_participant_id=payload_as_dict["conversation_message"]["author"]["id"],
+            sent_at=created_at,
+            content=payload_as_dict["conversation_message"]["body"],
+        )] + [ InternalMessage(
+            id=cp["id"],
+            author_participant_id=cp["author"]["id"],
+            sent_at=datetime.fromtimestamp(cp["created_at"]),
+            content=cp["body"],
+        )
+        for cp in payload_as_dict["conversation_parts"]["conversation_parts"]
+        ]
+
+    return InternalConversation(
+        id=id,
+        provider=provider,
+        external_id=external_id,
+        created_at=created_at,
+        updated_at=updated_at,
+        participants=participants,
+        messages=messages,
+    )
     raise NotImplementedError
--- app/api/routers/integrations_intercom.py
+++ app/api/routers/integrations_intercom.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
 from app.models.external.intercom import IntercomConversationRaw
 from app.services.ingestion import IngestResponse
+from app.adapters.intercom.mapper import map_intercom_to_internal
 
 router = APIRouter(prefix="/integrations/intercom", tags=["integrations"])
 
 
 @router.post("/conversations", response_model=IngestResponse)
 def ingest_intercom_conversation(payload: IntercomConversationRaw) -> IngestResponse:
+    internal_conversation = map_intercom_to_internal(payload)
+    print(f"internal convo: {internal_conversation}")
     return IngestResponse(
         id="00000000-0000-0000-0000-000000000000",
         provider="intercom",
